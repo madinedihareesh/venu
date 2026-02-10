d79b41cac74b430bae5e0623a9b56319b87f13ca
Hareesh <hareesh.madinedi@gmail.com>
2026-02-10 19:26:14 +0530

    day1

---
+++ Day1_Variable/ex.py
@@ -0,0 +1,52 @@
+'''
+Docstring for Day1_Variable.ex
+
+'''
+'''
+print('Hi venu welcome to session')
+name='venus'
+print(name)
+print(id(name)) 
+print(name.__sizeof__())
+
+name='Kumar'
+print(id(name))
+print(name.__sizeof__())
+print(dir(10))
+print(type(name))
+'''
+# variable diclarations
+'''
+_1name='vanu'
+name='kumar'
+Name='Krishna'
+NAME='Pavan'
+print(_1name,name,Name,NAME)
+'''
+# Data types (primitive)
+'''
+int
+float
+bool
+complex
+string
+'''
+i=10
+print(type(i))
+f=10.25
+print(type(f))
+s='sting'
+print(type(s))
+b=True
+print(type(b))
+c=10+11j
+print(type(c))
+
+
+print(hex(10))
+print(oct(10))
+print(int(10))
+
+s1=float(10)
+print(s1)
+print(type(s1))
